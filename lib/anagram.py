1c97baf9ffaa847233ae20ad3190c00424c6fb64
Hollis Crowder <hollis.crowder@gmail.com>
2024-04-24 14:26:13 -0700

    complete anagram lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, checklist):
+        matches = []
+        for item in checklist:
+            if sorted(list(self.word)) == sorted(list(item)):
+                matches.append(item)
+
+        return matches
+
+
+test = Anagram("listen")
+print(test.match(["enlist", "weather"]))
